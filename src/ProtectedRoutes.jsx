0c16cebfc5c788f1720ecc4e09a2bc6e8cdb9207
Shehab Shalan <shehab.shalan@hotmail.com>
2022-05-02 14:44:10 +0200

    cleaned code

--- src/ProtectedRoutes.jsx
+++ src/ProtectedRoutes.jsx
@@ -3,20 +3,17 @@ import { Route, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import { Container } from "@mui/material";
 import { useUserAuth } from "../src/context/UserAuthContext";
-function ProtectedRoutes({ component: Component, ...rest }) {
+function ProtectedRoutes({ component: Component }) {
   const { user } = useUserAuth();
-
   return (
     <Route
-      {...rest}
-      render={(props) => {
+      render={() => {
         if (!user) {
           return <Redirect to="/login" />;
         }
         return (
           <>
             <Header />
-
             <Container sx={{ py: 8 }} maxWidth="lg">
               <Component />
             </Container>
